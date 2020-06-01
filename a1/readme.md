fdae7f3e29cd8126592d9b755033b8fb0b806bd9
Mercury2699 <junjie526@outlook.com>
2020-06-01 00:28:04 -0400

    A1 Implement RegEx matching.

--- a1/readme.md
+++ a1/readme.md
@@ -2,3 +2,9 @@ Junjie He
 20739446 j247he
 openjdk version "11.0.7" 2020-04-14
 macOS 10.15.5 (19F96) MacBookPro13,2
+
+
+References:
+
+https://docs.oracle.com/javase/7/docs/api/java/io/File.html#list(java.io.FilenameFilter)
+https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
--- a1/src/FindFiles.java
+++ a1/src/FindFiles.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.lang.String;
 import java.io.File;
+import java.util.regex.Pattern;
 
 public class FindFiles {
     static boolean RegEx = false, Recursive = false;
@@ -18,7 +19,7 @@ public class FindFiles {
     }
 
     // Build a dictionary of key:value pairs (without the leading "-")
-    static HashMap<String, String> parse(String[] args) {
+    static HashMap<String, String> parse(String[] args){
         HashMap<String, String> arguments = new HashMap<>();
         String key = null, value = null;
         FileName = args[0];
@@ -42,27 +43,31 @@ public class FindFiles {
                 } else if (key.equals("reg")){
                     RegEx = true;
                     key = null;
-                } else if (key.equals("dir")){
-                    continue;
-                } else if (key.equals("ext")){
+                } else if (key.equals("dir") || key.equals("ext")){
                     continue;
                 } else {
-                    System.out.println("Invalid argument: " + key + ". Exiting. Use \"-help\" for usage. ");
+                    System.out.println("Invalid argument: " + key + ". Exiting.");
+                    printHelp();
                     System.exit(1);
                 }
             } else {
-                if (value != null) System.out.println("Duplicate value found. Skipping value " + value);
+                if (value != null){
+                    System.out.println(args[i] + " is not a valid argument. ");
+                }
                 value = args[i];
                 if (i == 0) value = null;
             }
-            if (key != null && value != null) {
+            if (key != null && value != null){
                 arguments.put(key, value);
                 key = null;
                 value = null;
             }
         }
         //  check final values
-        if (key != null) System.out.println("No argument provided after -" + key + ".");
+        if (key != null) {
+            System.out.println("No argument provided after -" + key + ".");
+            printHelp();
+        }
         if (value != null) System.out.println("No key for value " + value);
 
         return arguments;
@@ -75,27 +80,53 @@ public class FindFiles {
             // extract options from command line
             java.util.HashMap<String, String> options = parse(args);
             // print them to verify that it worked
-            for (Map.Entry entry : options.entrySet()) {
+            for (Map.Entry entry : options.entrySet()){
                 if (entry.getKey().equals("ext")) Extension = entry.getValue().toString().split(",");
                 if (entry.getKey().equals("dir")) Directory = entry.getValue().toString();
             }
 
-            System.out.println("FileName " + FileName);
-            System.out.println("Recursive " + Recursive);
-            System.out.println("RegEx " + RegEx);
-            System.out.println("Directory " + Directory);
-            System.out.println("Extension:");
-            for (String strings : Extension){
-                System.out.println(strings);
+            System.err.println("FileName " + FileName);
+            System.err.println("Recursive " + Recursive);
+            System.err.println("RegEx " + RegEx);
+            System.err.println("Directory " + Directory);
+            System.err.println("Extension:");
+            if (Extension != null) {
+                for (String strings : Extension){
+                    System.err.println(strings);
+                }
             }
 
-            System.out.println("Trying to find file named: " + FileName);
-            File search = new File(FileName);
-            if (!search.exists() || !search.isFile()){
-                System.out.println(args[0] + " is not found. ");
-                System.exit(0);
+            Boolean found = false;
+            try {
+                File dir = new File(Directory);
+                File[] files = dir.listFiles();
+                System.err.println("Trying to find file named: " + FileName + " in " + Directory);
+                File search = new File(FileName);
+                if (!dir.exists() || !dir.isDirectory()){
+                    System.out.println(Directory + " is not a valid directory. ");
+                    System.exit(0);
+                }
+                for (File aFile : files) {
+                    System.err.print(aFile.getName());
+                    if (RegEx) {
+                        Pattern p =Pattern.compile(FileName);
+                        System.err.println(p.matcher(aFile.getName()).matches());
+                        if (p.matcher(aFile.getName()).matches() && aFile.isFile()) {
+                            System.out.println("Found: " + aFile.getAbsolutePath());
+                            found = true;
+                        }
+                    } else {
+                        if (aFile.getName().equals(FileName) && aFile.isFile()) {
+                            System.out.println("Found: " + aFile.getAbsolutePath());
+                            found = true;
+                        }
+                    }
+                }
+                if (!found) System.out.print(FileName + " is not found in " + dir.getCanonicalPath());
+                if (Recursive) System.out.print("Recursively");
+            } catch (Exception ex){
+                System.out.println(ex.toString());
             }
-            System.out.println("Absolute path: " + search.getAbsolutePath());
         }
     }
 }
